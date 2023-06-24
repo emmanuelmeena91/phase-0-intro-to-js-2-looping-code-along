f556e8beb808b227a4a2087f640ed24dc6d91fee
emmanuelmeena91 <emmanuel.meena@moringaschool.com>
2023-06-24 10:17:13 +0300

    complete loops

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+function countDown(number){
+     // Keep looping as long as the number is greater than or equal to 0
+    while (number >= 0){
+        // Print the current value of the number
+        console.log(number);
+        number--; //Decrement by 1
+    }
+}
+console.log(countDown(10));
